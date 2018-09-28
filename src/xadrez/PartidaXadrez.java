a5f8c148a92027ee0d6b948a9a259ab90a83dca2
Jovino Joaquim <jovinojoaquim@hotmail.com>
2018-09-28 13:57:58 -0300

    Impressao do tabuleiro

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -8,7 +8,7 @@ import xadrez.pecas.Torre;
 
 public class PartidaXadrez {
 	private Tabuleiro tabuleiro;
-	
+
 	/**
 	 * Cria uma trix 8 x 8 e atribui o valor para a variável tabuleiro
 	 */
@@ -16,26 +16,36 @@ public class PartidaXadrez {
 		tabuleiro = new Tabuleiro(8, 8);
 		inicioPartida();
 	}
-	
-	public PecaXadrez[][] getPecas(){
+
+	public PecaXadrez[][] getPecas() {
 		PecaXadrez[][] partida = new PecaXadrez[tabuleiro.getLinhas()][tabuleiro.getColunas()];
-		for(int i =0; i<tabuleiro.getLinhas(); i++) {
-			for(int j = 0; j< tabuleiro.getColunas(); j++) {
-				partida[i][j] = (PecaXadrez) tabuleiro.peca(i, j); 
+		for (int i = 0; i < tabuleiro.getLinhas(); i++) {
+			for (int j = 0; j < tabuleiro.getColunas(); j++) {
+				partida[i][j] = (PecaXadrez) tabuleiro.peca(i, j);
 			}
 		}
 		return partida;
 	}
-	
+
 	/**
 	 * Método Responsável por inicializar as peças no tabuleiro
 	 */
 	private void inicioPartida() {
-		lugarNovoPeca('b', 6, new Torre(tabuleiro, Cor.BRANCO));
-		lugarNovoPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
-		lugarNovoPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
+		lugarNovoPeca('c', 1, new Torre(tabuleiro, Cor.BRANCO));
+		lugarNovoPeca('c', 2, new Torre(tabuleiro, Cor.BRANCO));
+		lugarNovoPeca('d', 2, new Torre(tabuleiro, Cor.BRANCO));
+		lugarNovoPeca('e', 2, new Torre(tabuleiro, Cor.BRANCO));
+		lugarNovoPeca('e', 1, new Torre(tabuleiro, Cor.BRANCO));
+		lugarNovoPeca('d', 1, new Rei(tabuleiro, Cor.BRANCO));
+		
+		lugarNovoPeca('c', 7, new Torre(tabuleiro, Cor.PRETO));
+		lugarNovoPeca('c', 8, new Torre(tabuleiro, Cor.PRETO));
+		lugarNovoPeca('d', 7, new Torre(tabuleiro, Cor.PRETO));
+		lugarNovoPeca('e', 7, new Torre(tabuleiro, Cor.PRETO));
+		lugarNovoPeca('e', 8, new Torre(tabuleiro, Cor.PRETO));
+		lugarNovoPeca('d', 8, new Rei(tabuleiro, Cor.PRETO));
 	}
-	
+
 	private void lugarNovoPeca(char coluna, int linha, Peca peca) {
 		tabuleiro.posicaoPeca(peca, new XadrezPosicao(coluna, linha).paraPosicao());
 	}
